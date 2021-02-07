0b618ede42a0b4472eac1835958dffed06e73c1e
Roberto Sciortino <roberto.sciortino@mail.mcgill.ca>
2021-02-07 13:04:12 -0500

    Add BaseDT Classifier

---
+++ Assignment1/base_dt_classifier.py
@@ -0,0 +1,16 @@
+from sklearn.tree import DecisionTreeClassifier
+from data_helper import pre_process, plot, write_stats
+
+# Pre-process/Extract data
+x_train, x_test, y_train, y_test = pre_process(open('all_sentiment_shuffled.txt', encoding="utf8"))
+
+# Plot the class counts
+plot(y_train + y_test)
+
+# Define MNB Clf
+clf = DecisionTreeClassifier()
+clf.fit(x_train, y_train)
+y_pred = clf.predict(x_test)
+
+# Write stats to file
+write_stats(y_pred, y_test, 'BaseDT-Sentiment.txt')
