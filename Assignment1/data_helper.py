63523cae1463f69ecc36da9a67229a2806ef4b3e
Roberto Sciortino <roberto.sciortino@mail.mcgill.ca>
2021-02-07 13:03:36 -0500

    Add data pipeline

---
+++ Assignment1/data_helper.py
@@ -0,0 +1,70 @@
+import string
+import collections
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+
+SPLIT = 0
+
+# Pre-calculated to avoid 2N looping
+NUM_WORDS = 54090
+NUM_INSTANCES = 11914
+
+def pre_process(file):
+    global SPLIT, NUM_WORDS, NUM_INSTANCES
+
+    labels = []
+    features = [[0]*NUM_WORDS for _ in range(NUM_INSTANCES)]
+    word_hash = collections.defaultdict(int)
+
+    i = 0
+    for j, line in enumerate(file):
+        line = line.split()
+        if line[1] == 'pos':
+            labels.append(1)
+        else:
+            labels.append(0)
+        
+        line = line[3:]
+        for w in line:
+            w = w.translate(str.maketrans('', '', string.punctuation))
+            if w:
+                if w not in word_hash:
+                    features[j][i] += 1
+                    word_hash[w] = i
+                    i += 1
+                else:
+                    features[j][word_hash[w]] += 1
+
+    file.close()
+    SPLIT = int(0.8 * len(labels))
+    return features[:SPLIT], features[SPLIT:], labels[:SPLIT], labels[SPLIT:]
+
+def plot(labels):
+    pos = sum(labels)
+    classes = ['Positive', 'Negative']
+    dist = [pos, len(labels) - pos]
+
+    plt.bar(classes, dist)
+    plt.ylabel('Count')
+    plt.xlabel("Sentiment")
+    plt.title('Sentiment Count')
+    plt.show()
+
+def write_stats(y_pred, y_test, out_name):
+    global SPLIT
+
+    file = open(out_name, 'w')
+
+    file.write("Classification Report:\n")
+    file.write(f'{classification_report(y_test, y_pred, target_names=["Negative", "Positive"])}\n')
+
+    file.write("Confusion Matrix:\n")
+    file.write(f'{np.array2string(confusion_matrix(y_test, y_pred))}\n\n')
+
+    file.write('Instance #, Prediction:\n')
+    file.writelines(f'{i}, {n}\n' for i, n in enumerate(y_pred, SPLIT))
+
+    file.close()
