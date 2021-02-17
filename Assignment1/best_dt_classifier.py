895ef352fb8e2c39d770ed285c524944a9a659d3
Roberto Sciortino <roberto.sciortino@mail.mcgill.ca>
2021-02-17 16:54:44 -0500

    Added looping for best param selection

--- Assignment1/best_dt_classifier.py
+++ Assignment1/best_dt_classifier.py
@@ -1,3 +1,5 @@
+import itertools
+
 from sklearn.tree import DecisionTreeClassifier
 from data_helper import pre_process, plot, write_stats
 
@@ -7,10 +9,25 @@ x_train, x_test, y_train, y_test = pre_process(open('all_sentiment_shuffled.txt'
 # Plot the class counts
 plot(y_train + y_test)
 
-# Define MNB Clf
-clf = DecisionTreeClassifier(random_state=42)
-clf.fit(x_train, y_train)
-y_pred = clf.predict(x_test)
+# Define classifier parameters to test
+param_dict= {
+    "criterion" : ['gini','entropy'],
+    'min_samples_split': [5, 10],
+    'min_samples_leaf' : [3, 5],
+}
 
-# Write stats to file
-write_stats(y_pred, y_test, 'BestDT-Sentiment.txt')
+# Define classifier + Train & Predict
+for params in itertools.product(*param_dict.values()):
+    clf = DecisionTreeClassifier(criterion=params[0], \
+                                    splitter='best', \
+                                    max_depth=None, \
+                                    min_samples_split=params[1], \
+                                    min_samples_leaf=params[2], \
+                                    max_features=None,
+                                    random_state=0)
+
+    clf.fit(x_train, y_train)
+    y_pred = clf.predict(x_test)
+
+    # Write stats to file
+    write_stats(y_pred, y_test, 'BestDT-Sentiment', params)
