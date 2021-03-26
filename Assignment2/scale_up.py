e12b6a840af6265cd6fd691607d96ecfa0c25c37
Roberto Sciortino <roberto.sciortino@mail.mcgill.ca>
2021-03-26 19:29:15 -0400

    Added command line arguments

--- Assignment2/scale_up.py
+++ Assignment2/scale_up.py
@@ -1,3 +1,6 @@
+import os
+import argparse
+
 from random import shuffle
 
 from analysis import run_analysis
@@ -6,9 +9,23 @@ from puzzle_helper import write_analysis, extend_puzzles, \
                             make_puzzle_tuples, generate_puzzles
 
 
-PUZZLE_START_SIZE = 3
-PUZZLE_END_SIZE = 6
-NUM_PUZZLES = 20
+# Define and parse input arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('--start_size', help='Start size of a puzzle. (eg. 3 (for 3x3), 4 (for 4x4) etc.)',
+                    required=True)
+parser.add_argument('--end_size', help='End size of a puzzle. Must be greater than start size! (eg. 4 (for 4x4), 5 (for 5x5) etc.)',
+                    required=True)
+parser.add_argument('--num_puzzles', help='Number of puzzles to generate for each size batch. (eg. 5, 10, 20 etc)',
+                    required=True)
+
+args = parser.parse_args()
+
+if int(args.end_size) <= int(args.start_size):
+    raise Exception("End size must be larger than start size!")
+
+PUZZLE_START_SIZE = int(args.start_size)
+PUZZLE_END_SIZE = int(args.end_size)
+NUM_PUZZLES = int(args.num_puzzles)
 
 
 def run_scale_up(a_type, f, linear_puzzles, algo_set):
@@ -22,9 +39,16 @@ if __name__ == "__main__":
     # Generate puzzles
     _, linear_puzzles = generate_puzzles(PUZZLE_START_SIZE, NUM_PUZZLES)
 
+    # Algo to test
     algo_set = [('A_Star_H2', a_star, h2)]
 
-    f = open('scale_up_analysis', 'w')
-    run_scale_up('scale-up analysis', f, linear_puzzles, algo_set)
+    # Open output file
+    f = 'outputs//scale_up_analysis.txt'
+    os.makedirs(os.path.dirname(f), exist_ok=True)
+    f = open(f, 'w')
+
+    # Run scale up analysis
+    run_scale_up('scale_up_analysis', f, linear_puzzles, algo_set)
     print(f"Full scale-up analysis completed!")
+
     f.close()
