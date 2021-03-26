83fec38fb0906a34142033af32fadb78b45d448e
Roberto Sciortino <roberto.sciortino@mail.mcgill.ca>
2021-03-26 14:07:51 -0400

    Added cost to analyis output

--- Assignment2/puzzle_helper.py
+++ Assignment2/puzzle_helper.py
@@ -59,12 +59,14 @@ def write_analysis(file, out, name, num_puzzles, size):
         av_sol_p_l, av_search_p_l, av_ex_time, av_no_sol = out
 
     file.write(f"Performance of {name} on random set of {num_puzzles} puzzle(s) of size {size}x{size}:\n")
-    file.write(f"Total solution path length: {tot_sol_p_l} iterations\n")
-    file.write(f"Total search path length: {tot_search_p_l} iterations\n")
+    file.write(f"Total solution path length: {tot_sol_p_l} states\n")
+    file.write(f"Total search path length: {tot_search_p_l} states\n")
+    file.write(f"Total cost: {tot_sol_p_l-1} iterations\n")
     file.write(f"Total execution time: {tot_ex_time} seconds\n")
     file.write(f"Total no solution: {tot_no_sol}\n")
-    file.write(f"Average solution path length: {av_sol_p_l} iterations\n")
-    file.write(f"Average search path length: {av_search_p_l} iterations\n")
+    file.write(f"Average solution path length: {av_sol_p_l} states\n")
+    file.write(f"Average search path length: {av_search_p_l} states\n")
+    file.write(f"Average cost: {av_sol_p_l-1} iterations\n")
     file.write(f"Average execution time: {av_ex_time} seconds\n")
     file.write(f"Average no solution: {av_no_sol}\n")
     file.write('\n')
