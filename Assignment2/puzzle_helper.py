11f3d362426cf625756773d1852089b0b5ecb691
Roberto Sciortino <roberto.sciortino@mail.mcgill.ca>
2021-03-26 19:27:39 -0400

    Modified writing results to file & added optimality results

--- Assignment2/puzzle_helper.py
+++ Assignment2/puzzle_helper.py
@@ -1,3 +1,4 @@
+import os
 from random import shuffle
 
 
@@ -30,10 +31,10 @@ def get_next_states(puzzle):
 
 # Check goal puzzle
 def is_goal_puzzle(puzzle):
-    prev = 0
+    prev = 1
     for i in range(len(puzzle)):
         for j in range(len(puzzle)):
-            if puzzle[i][j] != prev + 1:
+            if puzzle[i][j] != prev:
                 return False
             prev += 1
     return True
@@ -53,23 +54,32 @@ def retrace_solution_path(puzzle, parents, success):
     return solution_space
 
 
-# Write sull analyis to file
-def write_analysis(file, out, name, num_puzzles, size):
+# Write optimality results to file
+def write_optimality(a_type, f, opt_results):
+    if a_type == 'analysis':
+        f.write("Optimality results (Fastest algo on each puzzle):\n")
+        for i, (name, time) in enumerate(opt_results, 1):
+            f.write(f"{i}. {name}: {time} seconds\n")
+        f.write('\n')
+
+
+# Write full analyis to file
+def write_analysis(f, out, name, num_puzzles, size):
     tot_sol_p_l, tot_search_p_l, tot_ex_time, tot_no_sol, \
         av_sol_p_l, av_search_p_l, av_ex_time, av_no_sol = out
 
-    file.write(f"Performance of {name} on random set of {num_puzzles} puzzle(s) of size {size}x{size}:\n")
-    file.write(f"Total solution path length: {tot_sol_p_l} states\n")
-    file.write(f"Total search path length: {tot_search_p_l} states\n")
-    file.write(f"Total cost: {tot_sol_p_l-1} iterations\n")
-    file.write(f"Total execution time: {tot_ex_time} seconds\n")
-    file.write(f"Total no solution: {tot_no_sol}\n")
-    file.write(f"Average solution path length: {av_sol_p_l} states\n")
-    file.write(f"Average search path length: {av_search_p_l} states\n")
-    file.write(f"Average cost: {av_sol_p_l-1} iterations\n")
-    file.write(f"Average execution time: {av_ex_time} seconds\n")
-    file.write(f"Average no solution: {av_no_sol}\n")
-    file.write('\n')
+    f.write(f"Performance of {name} on random set of {num_puzzles} puzzle(s) of size {size}x{size}:\n")
+    f.write(f"Total solution path length: {tot_sol_p_l} states\n")
+    f.write(f"Total search path length: {tot_search_p_l} states\n")
+    f.write(f"Total cost: {tot_sol_p_l-1} iterations\n")
+    f.write(f"Total execution time: {tot_ex_time} seconds\n")
+    f.write(f"Total no solution: {tot_no_sol}\n")
+    f.write(f"Average solution path length: {av_sol_p_l} states\n")
+    f.write(f"Average search path length: {av_search_p_l} states\n")
+    f.write(f"Average cost: {av_sol_p_l-1} iterations\n")
+    f.write(f"Average execution time: {av_ex_time} seconds\n")
+    f.write(f"Average no solution: {av_no_sol}\n")
+    f.write('\n')
 
 
 # Displays puzzle nicely in output file
@@ -81,7 +91,10 @@ def write_puzzle(file, state):
 
 # Write output to file
 def output(filename, success, out, out_name):
-    with open(filename+f'_{out_name}_path', 'w') as f:
+    filename = f'outputs//{filename}_{out_name}_path.txt'
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
+
+    with open(filename, 'w') as f:
         if not success:
             f.write("No solution found")
         else:
@@ -112,7 +125,7 @@ def generate_puzzles(size, num_puzzles):
     linear_puzzles = []
 
     p = [str(i+1) for i in range(size**2)]
-    with open("puzzles", 'w') as f:
+    with open("puzzles.txt", 'w') as f:
         for _ in range(num_puzzles):
             shuffle(p)
             f.write(' '.join(p)+'\n')
