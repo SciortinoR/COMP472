b637fa9b49330f7a33294a9baa02dc16833151c7
Roberto Sciortino <roberto.sciortino@mail.mcgill.ca>
2021-04-13 18:50:42 -0400

    PNT Game alpha-beta pruning algo

---
+++ Assignment3/helper.py
@@ -0,0 +1,112 @@
+import math
+
+
+# Gets next token removals
+def get_next_removals(n_tokens, n_taken_tokens, taken_tokens, last_move):
+    if n_taken_tokens == 0:
+        return [i for i in range(1, (n_tokens+1)//2, 2)]
+
+    next_removals = get_factors(last_move, taken_tokens) + get_multiples(last_move, n_tokens, taken_tokens)
+    
+    return next_removals
+
+
+# Gets all multiples of last move
+def get_multiples(last_move, n_tokens, taken_tokens):
+    multiples = []
+    for i in range(last_move*2, n_tokens+1, last_move):
+        if not taken_tokens[i]:
+            multiples.append(i)
+
+    return multiples
+
+
+# Finds all factors of last move
+def get_factors(last_move, taken_tokens):
+    factors = []
+    for n in range(1, int(last_move**(0.5))+1):
+        if last_move % n == 0:
+            first = n
+            second = last_move // n
+
+            if not taken_tokens[first]:
+                factors.append(first)
+
+            if first != second and not taken_tokens[second]:
+                factors.append(second)
+
+    return factors
+
+
+# Finds largest prime factor of number
+def max_prime_factor(n):
+    max_prime = -1
+      
+    while n % 2 == 0:
+        n >>= 1
+          
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
+        while n % i == 0:
+            max_prime = i
+            n /= i
+
+    if n > 2:
+        max_prime = n
+
+    return int(max_prime)
+
+
+# Detrmines if number is prime
+def is_prime(n):
+    if (n <= 1) :
+        return False
+    if (n <= 3) :
+        return True
+
+    if (n % 2 == 0 or n % 3 == 0) :
+        return False
+ 
+    i = 5
+    while(i * i <= n) :
+        if (n % i == 0 or n % (i + 2) == 0) :
+            return False
+        i = i + 6
+ 
+    return True
+
+
+# Static Board Evaluation
+def board_eval(last_move, taken_tokens, next_removals, is_max_turn):
+    res = 0.0
+    
+    if len(next_removals) == 0:
+        res = -1.0
+    elif not taken_tokens[1]:
+        res = 0.0
+    elif last_move == 1:
+        res = 0.5 if len(next_removals)%2 != 0 else -0.5
+    elif is_prime(last_move):
+        # 1 is already taken, and last move is prime, therefore next removals are only multiples
+        res = 0.7 if len(next_removals) % 2 != 0 else -0.7
+    else:
+        num_multiples = 0
+        max_prime = max_prime_factor(last_move)
+
+        for num in next_removals:
+            if num % max_prime == 0:
+                num_multiples += 1
+
+        res = 0.6 if num_multiples % 2 != 0 else -0.6
+
+    return res if is_max_turn else -res
+
+# Prints all stats
+def print_stats(best_move, value, stats):
+    print(f'Move: {best_move}')
+    print(f'Value: {value}')
+    print(f"Number of Nodes Visited: {stats['nodes_vis']}")
+    print(f"Number of Nodes Evaluated: {stats['nodes_eval']}")
+    print(f"Max Depth Reached: {stats['max_depth']}")
+    print(f"Avg Effective Branching Factor: {0 if len(stats['branching_factors']) == 0 else sum(stats['branching_factors']) / len(stats['branching_factors']):.1f}")
+    print()
+    print(f"Execution time: {stats['end_time'] - stats['start_time']} seconds")
---
+++ Assignment3/main.py
@@ -0,0 +1,116 @@
+import sys
+import time
+
+from helper import get_next_removals, board_eval, print_stats
+
+
+# Start PNT Game
+def alpha_beta_search(n_tokens, n_taken_tokens, taken_tokens, last_move, max_depth, stats):
+    value, best_move = None, None
+
+    if n_taken_tokens % 2 == 0:
+        value, best_move = max_value(n_tokens, n_taken_tokens, taken_tokens, last_move, 0, max_depth, float('-inf'), float('inf'), stats)
+    else:
+        value, best_move = min_value(n_tokens, n_taken_tokens, taken_tokens, last_move, 0, max_depth, float('-inf'), float('inf'), stats)
+
+    return best_move, value
+
+
+# Plays Max Player turn
+def max_value(n_tokens, n_taken_tokens, taken_tokens, last_move, depth, max_depth, alpha, beta, stats):
+    stats['max_depth'] = max(stats['max_depth'], depth)
+    stats['nodes_vis'] += 1
+
+    next_removals = get_next_removals(n_tokens, n_taken_tokens, taken_tokens, last_move)
+    if len(next_removals) == 0 or (max_depth != 0 and depth == max_depth):
+        stats['nodes_eval'] += 1
+        return board_eval(last_move, taken_tokens, next_removals, True), None
+    
+    v = float('-inf')
+    best_move = None
+
+    branches_visited = 0
+
+    for move in next_removals:
+        taken_tokens[move] = True
+
+        v2, _ = min_value(n_tokens, n_taken_tokens + 1, taken_tokens, move, depth + 1, max_depth, alpha, beta, stats)
+
+        branches_visited += 1
+
+        if (v2 > v) or (v2 == v and (best_move is None or move < best_move)):
+            v, best_move = v2, move
+            alpha = max(alpha, v)
+
+        # Backtrack next removal tokens
+        taken_tokens[move] = False
+
+        if v >= beta:
+            break
+
+    stats['branching_factors'].append(branches_visited)
+    return v, best_move
+
+
+# Plays Min Player turn
+def min_value(n_tokens, n_taken_tokens, taken_tokens, last_move, depth, max_depth, alpha, beta, statistics):
+    stats['max_depth'] = max(stats['max_depth'], depth)
+    stats['nodes_vis'] += 1
+
+    next_removals = get_next_removals(n_tokens, n_taken_tokens, taken_tokens, last_move)
+    if len(next_removals) == 0 or (max_depth != 0 and depth == max_depth):
+        stats['nodes_eval'] += 1
+        return board_eval(last_move, taken_tokens, next_removals, False), None
+    
+    v = float('inf')
+    best_move = None
+
+    branches_visited = 0
+
+    for move in next_removals:
+        taken_tokens[move] = True
+
+        v2, _ = max_value(n_tokens, n_taken_tokens + 1, taken_tokens, move, depth + 1, max_depth, alpha, beta, statistics)
+
+        branches_visited += 1
+
+        if (v2 < v) or (v2 == v and (best_move is None or move < best_move)):
+            v, best_move = v2, move
+            beta = min(beta, v)
+
+        # Backtrack next removal tokens
+        taken_tokens[move] = False
+
+        if v <= alpha:
+            break
+    
+    stats['branching_factors'].append(branches_visited)
+    return v, best_move
+
+
+if __name__ == '__main__':
+    n_tokens = int(sys.argv[1])
+    n_taken_tokens = int(sys.argv[2])
+
+    # Use boolean list to denote taken tokens
+    taken_tokens = [False]*(n_tokens+1)
+    for i in range(n_taken_tokens):
+        taken_tokens[int(sys.argv[3+i])] = True
+    
+    depth = int(sys.argv[-1])
+    last_move = None if n_taken_tokens == 0 else int(sys.argv[-2])
+
+    stats = {
+        'nodes_vis': 0,
+        'nodes_eval': 0,
+        'max_depth': 0,
+        'branching_factors': [],
+        'start_time': time.time(),
+        'end_time': None
+    }
+
+    # Run alpha beta pruning
+    best_move, value = alpha_beta_search(n_tokens, n_taken_tokens, taken_tokens, last_move, depth, stats)
+    
+    stats['end_time'] = time.time()
+    print_stats(best_move, value, stats)
